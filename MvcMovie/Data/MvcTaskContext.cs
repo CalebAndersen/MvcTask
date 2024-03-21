e3b7e5f37acbe047df1897a42b36d36fa2f29b20
ST10251423 <st10251423@vcconnect.edu.za>
2024-03-21 17:55:49 +0200

    Add project files.

---
+++ MvcMovie/Data/MvcTaskContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcTasks.Models;
+
+namespace MvcTask.Data
+{
+    public class MvcTaskContext : DbContext
+    {
+        public MvcTaskContext (DbContextOptions<MvcTaskContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcTasks.Models.Task> Task { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MvcTasks.Models.Task>()
+                .Ignore(t => t.timerEnded);
+        }
+    }
+}
---
+++ MvcMovie/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcTasks.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime Deadline { get; set; }
+    public string? Status { get; set; }
+
+    public bool timerEnded { get; set; } = false;
+}
